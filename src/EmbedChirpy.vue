23848a5962e9a1c52e590e7fba3fdbe437c78e9e
Jason Ford <fordjason@gmail.com>
2025-04-29 16:54:25 -0700

    Proxy competency reports

--- src/EmbedChirpy.vue
+++ src/EmbedChirpy.vue
@@ -2,18 +2,40 @@
   import { ref } from 'vue'
   import { vueEmbedComponent } from '@knowlearning/agents/vue.js'
 
+  const copy = x => JSON.parse(JSON.stringify(x))
+
   const { id } = defineProps({ id: String })
 
   const chirpyLink = ref(null)
 
+  let gameId
+
   Agent
     .state(id)
     .then(async ({ game, configuration }) => {
+      gameId = game
       const { variables } = await Agent.environment()
       variables.configuration = configuration
       const lang = variables.LANGUAGES?.[0]?.split('-')[0] || 'en'
       chirpyLink.value = `https://pila.cand.li/pila-play.html?game=${game}&lang=${lang}`
     })
+
+  async function handleClose(info) {
+    if (Agent.embedded) {
+      await Agent.synced()
+      const latestCompetencies = await Agent.state(`pila/latest_competencies/${gameId}`)
+      Agent.close({ competencies: copy(latestCompetencies), success: info?.success })
+    }
+    else {
+      closeInfo.value = info
+    }
+    await clearOutLatestCompetencies()
+  }
+
+  async function clearOutLatestCompetencies() {
+    const competencies = await Agent.state(`pila/latest_competencies/${gameId}`)
+    Object.keys(competencies).forEach(key => delete competencies[key])
+  }
 </script>
 
 <template>
@@ -27,6 +49,7 @@
     <vueEmbedComponent
       v-if="chirpyLink"
       :id="chirpyLink"
+      @close="handleClose"
     />
   </div>
 </template>
