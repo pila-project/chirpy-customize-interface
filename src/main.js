d2a557178c4204e346aea408e499b09fe1fe770a
Matthew McClain <mthommcclain@gmail.com>
2025-10-15 16:46:20 -0700

    set favicon based on route, uuid type

--- src/main.js
+++ src/main.js
@@ -11,9 +11,25 @@ window.Agent = Agent
 const pathSegment = location.pathname.slice(1)
 
 if (isUUID(pathSegment)) {
+
+	// for favicon, gotta grab data of uuid. annoying
+	// this image is a url, but I'll just use it to know what type it is and point to public
+	const { image } = await Agent.state(pathSegment)
+	setFavicon(image.contains('Chirpy') ? '/Chirpy.png' : '/Mixology.png')
+
 	createApp(EmbedCandli, { id: pathSegment }).mount('#app')
 } else if (pathSegment === 'chirpy' || pathSegment === 'mixology') {
+	setFavicon(pathSegment === 'chirpy' ? '/Chirpy.png' : '/Mixology.png')
 	createApp(App, { path: pathSegment }).mount('#app')
 } else {
 	createApp(NotFound).mount('#app')
+}
+
+function setFavicon(path) {
+  const link =
+    document.querySelector("link[rel~='icon']") ||
+    document.createElement('link')
+  link.rel = 'icon'
+  link.href = path
+  document.head.appendChild(link)
 }
